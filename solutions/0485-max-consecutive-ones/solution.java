69eb54b6b84d04255b83a66c3da56e5bd487868e
Adnan Wazwaz <73372148+adnanian@users.noreply.github.com>
2025-12-20 15:10:44 +0000

    Sync LeetCode submission Runtime - 2 ms (97.54%), Memory - 52.8 MB (11.92%)

--- solutions/0485-max-consecutive-ones/solution.java
+++ solutions/0485-max-consecutive-ones/solution.java
@@ -1,3 +1,4 @@
+// Iteration 2
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         // Current max number of consecutive 1's.
@@ -5,10 +6,32 @@ class Solution {
         // Current number of consecutive 1's in the iteration process.
         int currentStreak = 0;
         for (int num: nums) {
-            currentStreak = (num == 1) ? currentStreak + 1 : 0;
-            maxStreak = Math.max(maxStreak, currentStreak);
+            if (num == 1) {
+                currentStreak++;
+            } else {
+                maxStreak = Math.max(maxStreak, currentStreak);
+                currentStreak = 0;
+            }
         }
+
+        maxStreak = Math.max(maxStreak, currentStreak);
 
         return maxStreak;
     }
 }
+
+// Iteration 1
+// class Solution {
+//     public int findMaxConsecutiveOnes(int[] nums) {
+//         // Current max number of consecutive 1's.
+//         int maxStreak = 0;
+//         // Current number of consecutive 1's in the iteration process.
+//         int currentStreak = 0;
+//         for (int num: nums) {
+//             currentStreak = (num == 1) ? currentStreak + 1 : 0;
+//             maxStreak = Math.max(maxStreak, currentStreak);
+//         }
+
+//         return maxStreak;
+//     }
+// }
